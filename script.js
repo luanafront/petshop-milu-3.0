7c6945b8597500c029eb01b588d5a19ac41c15c5
luanafront <luana.lopes.rodrigues1@gmail.com>
2022-02-25 18:45:27 -0300

    criei o botao hamburguer, personalizei com css e executei a funcion com js

---
+++ script.js
@@ -0,0 +1,30 @@
+/**
+ * fatia1-clicada
+fatia2-clicada
+fatia3-clicada
+ */
+const fatia1 = document.getElementById("fatia1")
+const fatia2 = document.getElementById("fatia2")
+const fatia3 = document.getElementById("fatia3")
+const botaoHamburguer = document.getElementById("botao-hamburguer")
+let estaClicado = false 
+
+function Clicar (){
+    if(estaClicado === false){
+        estaClicado = true
+    } else {
+        estaClicado = false 
+    } 
+
+    if(estaClicado === true){
+        fatia1.classList.add("fatia1-clicada")
+        fatia2.classList.add("fatia2-clicada")
+        fatia3.classList.add("fatia3-clicada")
+    } else {
+        fatia1.classList.remove("fatia1-clicada")
+        fatia2.classList.remove("fatia2-clicada")
+        fatia3.classList.remove("fatia3-clicada")
+    }
+}
+
+botaoHamburguer.addEventListener("click",Clicar)
